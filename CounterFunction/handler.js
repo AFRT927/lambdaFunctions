9cae8574776b248ebbb21933131edf79995b38e3
Andres <andrese-rios@utp.edu.co>
2023-06-29 10:34:19 -0500

    adding context info

--- CounterFunction/handler.js
+++ CounterFunction/handler.js
@@ -1,20 +1,16 @@
 
-exports.counter = async (event) => {
+exports.counter = async (event, context) => {
 
     console.log(`el numero enviado es ${event.pathParameters.id}`)
+
+    console.log('Remaining time: ', context.getRemainingTimeInMillis())
+    console.log('Function name: ', context.functionName)
     // agregamos logica basada en el parametro enviado
     // ojo, es una mala practica hacer esto ya que AWS recomienda
     // escribir funciones lambda statless
 
 let res
-    if(!isNaN(event.pathParameters.id)) {
 
-        let num = Number(event.pathParameters.id) + 1000
-        return {
-            statusCode: 200,
-            body: `el id es el numero ${num}`
-        }
-    }
     if(event.pathParameters.id === "crear"){
       res = "el dato se esta creando"
     } else if (event.pathParameters.id === "actualizar"){
@@ -25,6 +21,11 @@ let res
 
     return {
         statusCode: 200,
-        body: `*** ${res} ***`
+        body: {
+            res,
+            remT: context.getRemainingTimeInMillis(),
+            funN: context.functionName,
+            arn: context.invokedFunctionArn
+        }
     }
 }
